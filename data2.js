e759652aaa50c7128a670ec4049aa8869631175b
Sahil Rajput <sahilrajput03@gmail.com>
2022-09-17 22:21:23 +0530

    up

--- data2.js
+++ data2.js
@@ -1,13 +1,17 @@
 module.exports = [
 	//
-	'sanjiv42 snjvsharma42@gmail.com',
-	'Krushn kathrechakrushn@gmail.com',
-	'Nipan Das nipandas83@gmail.com',
-	'Debdut Goswami debdutgoswami@gmail.com',
-	'NIPAN DAS nipandas83@gmail.com',
-	'Deepak Bhattarai bring2dip@gmail.com',
-	'claudionts claudionts@gmail.com',
-	'Gabriel Lacerda gabriellacerdadesign@gmail.com',
-	'GabrielLacerda gabriellacerdadesign@gmail.com',
-	'knightriser gabriellacerdadest@gmail.com',
+	// 'sanjiv42 snjvsharma42@gmail.com',
+	// 'Krushn kathrechakrushn@gmail.com',
+	// 'Nipan Das nipandas83@gmail.com',
+	// 'Debdut Goswami debdutgoswami@gmail.com',
+	// 'NIPAN DAS nipandas83@gmail.com',
+	// 'Deepak Bhattarai bring2dip@gmail.com',
+	// 'claudionts claudionts@gmail.com',
+	// 'Gabriel Lacerda gabriellacerdadesign@gmail.com',
+	// 'GabrielLacerda gabriellacerdadesign@gmail.com',
+	// 'knightriser gabriellacerdadest@gmail.com',
+	// "Lazarus Chisom Nwankwo lazicah@gmail.com"
+	// "Sifon Isaac syfonisaac@gmail.com",
+	// "Bryan Ezeaka nwanne.ezeaka@gmail.com",
+	"Habeeb Ahmadu habeebahmadu1@gmail.com"
 ]
